35c808f3234309ca60a1b8917b78324e8223b81c
MostafaOwies <mostafaowies@yahoo.com>
2021-07-11 15:28:22 +0200

    my currency converter app

---
+++ app/src/main/java/com/example/currencyconverterapp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.currencyconverterapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void convert(View view){
+        EditText input= findViewById(R.id.input);
+        TextView output = findViewById(R.id.output);
+        String usd = input.getText().toString();
+
+        double usddouble = Double.parseDouble(usd);
+        double egpdouble = usddouble*15.66;
+
+        String egp = String.valueOf(egpdouble);
+        Log.i("test","test");
+
+        Toast.makeText(this, egp + " EGP £", Toast.LENGTH_SHORT).show();
+
+        output.setText(egp + " EGP £");
+
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
